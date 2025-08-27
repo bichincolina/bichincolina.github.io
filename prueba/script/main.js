ef1771c8a7187b905f2f4c94447a23b103fa3347
bichincolina <166668452+bichincolina@users.noreply.github.com>
2025-08-27 10:18:49 -0400

    Add files via upload

---
+++ prueba/script/main.js
@@ -0,0 +1,9 @@
+let message = "hola desde js";
+document.writeln("<h3>"+ message + "</h3>");
+
+function suma(a, b) {
+    return a + b;
+}
+const timestamp = Date.now();
+message = "hoy es timestamp: " + timestamp;
+document.writeln("<p>"+ message + "<p>");
