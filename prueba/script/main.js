cf5846173498d17c0964f01798a3a7bc93d7e5e7
bichincolina <166668452+bichincolina@users.noreply.github.com>
2025-08-28 14:55:57 -0400

    Update main.js

--- prueba/script/main.js
+++ prueba/script/main.js
@@ -1,9 +1,78 @@
 let message = "hola desde js";
 document.writeln("<h3>"+ message + "</h3>");
 
-function suma(a, b) {
-    return a + b;
+// function suma(a, b) {
+//     a = 10;
+//     b = 20;
+//     return a + b;
+// }
+// function resta(a, b) {
+//     return a - b;
+// }
+// function nombreCompleto(nombre, apellido) {
+//     return nombre + " " + apellido;
+// }
+
+// Generic
+// function getAge(edad) {
+//     var formData = new FormData(edad);
+//     let age = Object.fromEntries(formData)["edad1"];
+//     // console.log(Object.fromEntries(formData));
+//     // console.log(JSON.stringify(Object.fromEntries(formData), null, 1));
+//     let msg;
+//     switch (true) {
+//         case age < 18:
+//             msg = "You are underaged! Get out...";
+//             break;
+//         case age < 65:
+//             msg = "You are an Adult!";
+//             break;
+//         case age < 85:
+//             msg = "You are a Senior!";
+//             break;
+//         default:
+//             msg = "You are in your Golden Years! :>"
+//             break;
+//     }
+//     document.getElementById("edad").innerHTML += msg;
+// }
+
+ 
+// document.getElementById("edad").addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     getAge(e.target);
+// });
+
+function getAge(edad) {
+    var formData = new FormData(edad);
+    let age = Object.fromEntries(formData)["edad1"];
 }
-const timestamp = Date.now();
-message = "hoy es timestamp: " + timestamp;
-document.writeln("<p>"+ message + "<p>");
+function mayorDeEdad(e) {
+    if ( e>=0) {
+        alert("eres mayor de cero");
+        return true;
+    } else {
+        alert("eres menor de cero");
+        return false;
+    }   
+}
+document.getElementById("edad").addEventListener("submit", function (e){
+    e.preventDefault();
+    var formData = new FormData(e.target);
+    console.log(e);
+    console.log(Object.fromEntries(formData)["edad1"]);
+    if(mayorDeEdad(Object.fromEntries(formData)["edad1"]) ){
+        alert("eres mayor de cero");
+        console.log("eres mayor de cero");
+    } else {
+        alert("eres menor de cero");
+        console.log("eres menor de cero");
+    }});
+// const submit = document.querySelector("submit");
+// const timestamp = Date.now();
+// message = "hoy es timestamp: " + timestamp;
+// document.writeln("<p>"+ message + "<p>");
+// document.writeln("<p>la suma es: " + suma() + "</p>");
+// document.writeln("<p>la resta es: " + resta(50, 20) + "</p>");
+// document.writeln("<p>mi nombre es: " + nombreCompleto("bichi","colina") + "</p>");
+// document.writeln("<p> edad " + mayorDeEdad("su edad es") + "</p>");
