173c8328469161ca66ef8eea66960df2ad2730bd
bichincolina <bichincolina1@gmail.com>
2025-11-02 21:40:07 -0300

    client

--- ev2/elitaliano/app/Login/page.tsx
+++ ev2/elitaliano/app/Login/page.tsx
@@ -1,8 +1,6 @@
-// app/Login/page.tsx
 "use client";
 import React from "react";
 import Login from "./login";
-
 
 
 export default function Page() {
--- ev2/elitaliano/app/Registro/page.tsx
+++ ev2/elitaliano/app/Registro/page.tsx
@@ -1,4 +1,3 @@
-// app/Registro/page.tsx
 "use client";
 import React from "react";
 import Registro from "./registro";
